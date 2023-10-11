fe2b7c4d01eb1b17db0184a2bdf0a9aa78946723
sigvebjo <sigve@bjorkedal.org>
2023-10-11 09:40:10 +0200

    Update BST, missing format, insert invalid

--- src/main/java/no/ntnu/idata2302/lab04/BST.java
+++ src/main/java/no/ntnu/idata2302/lab04/BST.java
@@ -2,160 +2,187 @@ package no.ntnu.idata2302.lab04;
 
 public class BST {
 
-    public static BST fromValues(int... values) {
-        if (values.length < 1)
-            throw new IllegalArgumentException("A binary search tree must have at least one value");
-        if (values.length == 1)
-            return new BST(values[0]);
-        var tree = new BST(values[0]);
-        for (int i = 1; i < values.length; i++) {
-            tree.insert(values[i]);
-        }
-        return tree;
-    }
+	public static BST fromValues(int... values) {
+		if (values.length < 1)
+			throw new IllegalArgumentException("A binary search tree must have at least one value");
+		if (values.length == 1)
+			return new BST(values[0]);
+		var tree = new BST(values[0]);
+		for (int i = 1; i < values.length; i++) {
+			tree.insert(values[i]);
+		}
+		return tree;
+	}
 
-    private int value;
-    private BST left;
-    private BST right;
+	private int value;
+	private BST left;
+	private BST right;
 
-    public BST(int value) {
-        this.value = value;
-    }
+	public BST(int value) {
+		this.value = value;
+	}
 
-    public BST insert(int givenValue) {
-        // TODO: Implement this operation
-        throw new RuntimeException("Not yet implemented");
-    }
+	public BST insert(int givenValue) {
+		if (givenValue == value) {
+			throw new IllegalArgumentException("Cannot insert a duplicate value");
+		} else if (givenValue < value) {
+			if (this.hasLeft()) {
+				this.left.insert(givenValue);
+			} else {
+				this.left = new BST(givenValue);
+			}
+		} else {
+			if (this.hasRight()) {
+				this.right.insert(givenValue);
+			} else {
+				this.right = new BST(givenValue);
+			}
+		}
 
-    /**
-     * @return the number of items in this tree
-     */
-    public int size() {
-        // TODO: implement this operation
-        throw new RuntimeException("Not yet implemented!");
-    }
+		// What do you want us to return
+		return this;
+	}
 
-    private boolean hasLeft() {
-        return left != null;
-    }
+	/**
+	 * @return the number of items in this tree
+	 */
+	public int size() {
+		int size = 1;
+		if (hasLeft()) {
+			size += this.left.size();
+		}
+		if (hasRight()) {
+			size += this.right.size();
+		}
+		return size;
+	}
 
-    private boolean hasRight() {
-        return right != null;
-    }
+	private boolean hasLeft() {
+		return left != null;
+	}
 
-    int minimum() {
-        // TODO: Implement this operation
-        throw new RuntimeException("Not yet implemented!");
+	private boolean hasRight() {
+		return right != null;
+	}
 
-    }
+	int minimum() {
+		if (this.hasLeft()) {
+			return this.left.minimum();
+		} else {
+			return this.value;
+		}
+	}
 
-    int maximum() {
-        // TODO: Implement this operation
-        throw new RuntimeException("Not yet implemented!");
+	int maximum() {
+		if (this.hasRight()) {
+			return this.right.minimum();
+		} else {
+			return this.value;
+		}
 
-    }
+	}
 
-    boolean contains(int givenValue) {
-        if (value < givenValue) {
-            if (hasRight()) {
-                return right.contains(givenValue);
-            }
-            return false;
+	boolean contains(int givenValue) {
+		if (value < givenValue) {
+			if (hasRight()) {
+				return right.contains(givenValue);
+			}
+			return false;
 
-        } else if (value > givenValue) {
-            if (hasLeft()) {
-                return left.contains(givenValue);
-            }
-            return false;
+		} else if (value > givenValue) {
+			if (hasLeft()) {
+				return left.contains(givenValue);
+			}
+			return false;
 
-        } else {
-            return true;
+		} else {
+			return true;
 
-        }
-    }
+		}
+	}
 
-    int successor(int givenValue) {
-        if (value < givenValue) {
-            if (hasRight()) {
-                return right.successor(givenValue);
+	int successor(int givenValue) {
+		if (value < givenValue) {
+			if (hasRight()) {
+				return right.successor(givenValue);
 
-            }
-            throw new NoSuchValue(givenValue);
+			}
+			throw new NoSuchValue(givenValue);
 
-        } else if (value > givenValue) {
-            if (hasLeft()) {
-                try {
-                    return left.successor(givenValue);
+		} else if (value > givenValue) {
+			if (hasLeft()) {
+				try {
+					return left.successor(givenValue);
 
-                } catch (SuccessorNotFound error) {
-                    return value;
+				} catch (SuccessorNotFound error) {
+					return value;
 
-                } catch (NoSuchValue error) {
-                    return value;
+				} catch (NoSuchValue error) {
+					return value;
 
-                }
-            }
-            throw new NoSuchValue(givenValue);
+				}
+			}
+			throw new NoSuchValue(givenValue);
 
-        } else {
-            if (hasRight()) {
-                return right.minimum();
+		} else {
+			if (hasRight()) {
+				return right.minimum();
 
-            }
-            throw new SuccessorNotFound();
-        }
-    }
+			}
+			throw new SuccessorNotFound();
+		}
+	}
 
-    BST delete(int givenValue) {
-        if (givenValue < value) {
-            if (hasLeft()) {
-                left = left.delete(givenValue);
-                return this;
-            }
-            throw new NoSuchValue(givenValue);
-        } else if (givenValue > value) {
-            if (hasRight()) {
-                right = right.delete(givenValue);
-                return this;
-            }
-            throw new NoSuchValue(givenValue);
-        } else {
-            if (hasLeft() && !hasRight()) {
-                return left;
-            }
-            if (!hasLeft() && hasRight()) {
-                return right;
-            }
-            if (hasLeft() && hasRight()) {
-                try {
-                    value = successor(value);
+	BST delete(int givenValue) {
+		if (givenValue < value) {
+			if (hasLeft()) {
+				left = left.delete(givenValue);
+				return this;
+			}
+			throw new NoSuchValue(givenValue);
+		} else if (givenValue > value) {
+			if (hasRight()) {
+				right = right.delete(givenValue);
+				return this;
+			}
+			throw new NoSuchValue(givenValue);
+		} else {
+			if (hasLeft() && !hasRight()) {
+				return left;
+			}
+			if (!hasLeft() && hasRight()) {
+				return right;
+			}
+			if (hasLeft() && hasRight()) {
+				try {
+					value = successor(value);
 
-                } catch (SuccessorNotFound error) {
-                    return null;
+				} catch (SuccessorNotFound error) {
+					return null;
 
-                }
-                right.delete(value);
-                return this;
-            }
-            return null;
-        }
-    }
+				}
+				right.delete(value);
+				return this;
+			}
+			return null;
+		}
+	}
 
-    public String format() {
-        // TODO: Implement this operation
-        throw new RuntimeException("Not yet implemented!");
-    }
+	public String format() {
+		// TODO: Implement this operation
+		throw new RuntimeException("Not yet implemented!");
+	}
 
 }
 
 class NoSuchValue extends RuntimeException {
 
-    private int value;
+	private int value;
 
-    public NoSuchValue(int givenValue) {
-        super();
-        this.value = givenValue;
-    }
+	public NoSuchValue(int givenValue) {
+		super();
+		this.value = givenValue;
+	}
 }
 
 class SuccessorNotFound extends RuntimeException {
