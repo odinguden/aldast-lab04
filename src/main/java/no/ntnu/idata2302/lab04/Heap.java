cb9ed836648a61d87a6d6e3491b6945d18e2edf3
Odin Lyngsgård <odin24@outlook.com>
2023-10-17 15:13:23 +0200

    Insert method implemented

--- src/main/java/no/ntnu/idata2302/lab04/Heap.java
+++ src/main/java/no/ntnu/idata2302/lab04/Heap.java
@@ -1,54 +1,94 @@
 package no.ntnu.idata2302.lab04;
 
 import java.util.ArrayList;
-
+/**
+ * This class represents a minimum heap
+ */
 public class Heap {
+	
+	public static Heap fromValues(int... values) {
+		var heap = new Heap();
+		for (var each : values) {
+			heap.insert(each);
+		}
+		return heap;
+	}
 
-    public static Heap fromValues(int... values) {
-        var heap = new Heap();
-        for (var each : values) {
-            heap.insert(each);
-        }
-        return heap;
-    }
+	private ArrayList<Integer> array;
 
-    private ArrayList<Integer> array;
+	public Heap() {
+		array = new ArrayList<Integer>();
+		
+	}
 
-    public Heap() {
-        array = new ArrayList<Integer>();
-        array.add(0);
-    }
+	/**
+	 * homie.
+	 * 
+	 * @param k
+	 */
+	public void insert(Integer k) {
+		array.add(k);
+		int pos = array.size() - 1;
 
-    public void insert(Integer k) {
-        // TODO: Implement this operation
-        throw new RuntimeException("Not yet implemented");
-    }
 
-    public int takeMinimum() {
-        // TODO: Implement this operation
-        throw new RuntimeException("Not yet implemented");
-    }
+		heapify(pos);
 
-    public void decreaseKey(int i, int k) {
-        // TODO: Implement this operation
-        throw new RuntimeException("Not yet implemented");
-    }
+	}
 
-    private int parentOf(int index) {
-        return index / 2;
-    }
+	public void heapify (int pos) {
 
-    private int leftChildOf(int index) {
-        return index * 2;
-    }
+		if (pos == 0) {
+			return;
+		}
 
-    private int rightChildOf(int index) {
-        return index * 2 + 1;
-    }
+		int parentPos = parentOf(pos);
 
-    void swap(int pos1, int pos2) {
-        int temp = array.get(pos1);
-        array.set(pos1, array.get(pos2));
-        array.set(pos2, temp);
-    }
+		if ( array.get(pos) < array.get(parentPos)) {
+			swap(pos, parentOf(pos));
+
+			heapify(parentOf(pos));
+		}
+	}
+
+	/**
+	 * Returns the smallest item
+	 * 
+	 * @return the smallest item in the heap
+	 */
+	public int takeMinimum() {
+		int minimum = array.get(0);
+
+		
+
+		return minimum;
+	}
+
+	public void decreaseKey(int i, int k) {
+		// TODO: Implement this operation
+		throw new RuntimeException("Not yet implemented");
+	}
+
+	private int parentOf(int index) {
+		return index / 2;
+	}
+
+	private int leftChildOf(int index) {
+		return index * 2;
+	}
+	
+	private int rightChildOf(int index) {
+		return index * 2 + 1;
+	}
+
+	/**
+	 * Swaps the placement the items at pos1 and pos2 
+	 * 
+	 * @param pos1
+	 * @param pos2
+	 */
+	void swap(int pos1, int pos2) {
+		int temp = array.get(pos1);
+		array.set(pos1, array.get(pos2));
+		array.set(pos2, temp);
+	}
 }
